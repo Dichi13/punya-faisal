844c88c2c0d6e7b95d7ee357bc40cc20913f9480
Lutfi Maulana <lutfie_ultimateband@yahoo.co.id>
2022-04-13 13:34:25 +0700

    add converter

---
+++ Binus.BL.CertificateGenerator/Controllers/GeneratePdfController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Wkhtmltopdf.NetCore;
+
+namespace Binus.BL.CertificateGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GeneratePdfController : ControllerBase
+    {
+        private IGeneratePdf _genratePdf;
+
+        public GeneratePdfController(IGeneratePdf generatePdf)
+        {
+            _genratePdf = generatePdf;
+        }
+
+        [HttpPost]
+        public string GeneratePdf([FromBody] GeneratePdfDTO model)
+        {
+            var data = _genratePdf.GetPDF(model.HtmlContent);
+
+            return Convert.ToBase64String(data);
+        }
+
+        public class GeneratePdfDTO
+        {
+            public string HtmlContent { get; set; }
+        }
+    }
+}
